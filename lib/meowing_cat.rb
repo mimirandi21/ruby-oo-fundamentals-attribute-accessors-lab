27667651f769802adfc03a64e3bd5e90613dcea1
Randi Schreiner <mimirandi21@gmail.com>
2020-10-06 12:04:58 -0500

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,12 @@
 ## code your solution here. 
+
+#create Cat class
+class Cat
+    #creating getter & setter for name
+    attr_accessor :name
+    #writing meow method
+    def meow
+        puts "meow!"
+    end
+    
+end
